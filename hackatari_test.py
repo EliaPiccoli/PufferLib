f15670547f643f84fab0f95341eeca54d2dae952
Malio <malio.li@di.unipi.it>
2025-02-27 13:58:03 +0100

    hackatari evaluation fix

--- hackatari_test.py
+++ hackatari_test.py
@@ -1,7 +1,14 @@
+import cv2
 import hackatari
 from hackatari import HackAtari
-
+from gymnasium.wrappers import GrayScaleObservation
+import pufferlib
+from pufferlib.environments.atari.environment import AtariPostprocessor
 import pygame
+import torch
+import numpy as np
+
+from tqdm import tqdm
 
 """Modifications
 Breakout:
@@ -42,39 +49,114 @@ SpaceInvaders:
     - curved
     - relocateXX, X in {35,53}
 """
+def color_variations(num_comb=25):
+    val1 = range(5)
+    val2 = range(5)
+    
+    combinations = []
+    for v1 in val1:
+        for v2 in val2:
+            combinations.append(("color_p"+str(v1), "color_b"+str(v2)))
+    return combinations[:num_comb]
 
-env = HackAtari("Breakout", ["strength5",], render_mode="rgb_array") # human, rgb_array
+def env_creator(*args, **kwargs):
+    modification = []
+    name = "Breakout"
+    
+    if "modification" in kwargs:
+        modification = kwargs["modification"]
+    if "env_name" in kwargs:
+        name = kwargs["env_name"]
+    env = HackAtari(name, modification, obs_mode="ori", buffer_window_size=1, render_mode="rgb_array") # human, rgb_array
+    env = GrayScaleObservation(env)
+    env = pufferlib.postprocess.ResizeObservation(env, downscale=2)
+    env = AtariPostprocessor(env)
+    env = pufferlib.postprocess.EpisodeStats(env)
+    env = pufferlib.emulation.GymnasiumPufferEnv(env=env)
+    return env
 
 # define policy
-policy = None
+model = torch.load('/home/malio/PufferLib/experiments/breakout-663eb907/model_000382.pt', map_location='cuda:3', weights_only=False)
 
-pygame.init()
+model.eval()
+print("Model loaded")
+policy = model
 
-for i in range(10):
-    obss = []
-    obs, _ = env.reset()
-    done = False
-    nstep = 1
+# here define the environment name
+env_name = "Breakout"
+device = torch.device("cuda:3")
 
-    tr = 0
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-                done = True
+with open(f"stats_{env_name}.txt", "w") as f:
+    episodes = 10
+    
+    # here define the modification combinations 
+    # using  a function to generate them or
+    # manually write them 
+    combs = color_variations()
+    # combs = [("lazy_enemy","up_driftX1", "down_drift2"), 
+    #          ("lazy_enemy","up_driftX1", "down_drift2"),]
+    
+    print("total combinations: ", len(combs))
+    
+    for combination in combs:
+        episodic_returns = []
+        
+        name = f"{env_name}_"
+        for c in combination:
+            name += c + "_"
+        
+        f.write(f"Combination: {combination}\n")
+        
+        print("*"  * 50)
+        print(f"Combination: {combination}")
+        print("*"  * 50)
+        modification = combination
+        
+        env = env_creator( env_name=env_name)
+        driver = env
 
-        action = policy(torch.Tensor(obs).unsqueeze(0))[0] if policy else env.action_space.sample()
-        obs, reward, terminated, truncated, info = env.step(action)
-        tr += reward
+        pygame.init()
 
-        if reward:
-            print(reward)
+        # make video 
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(f'./videos/{name}.mp4',fourcc, 30.0, (160,210))
+        print(f"Starting {name}...")
 
-        if terminated or truncated:
-            print(info, tr)
-            tr = 0
-            env.reset()
+        f.write("*" * 50 )
+        with tqdm(total=episodes) as pbar:
+            for i in range(episodes):
+                obss = []
+                obs, _ = env.reset()
+                done = False
 
-        nstep += 1
-        env.render(env._state_buffer_rgb[-1])
+                episodic_return = 0
+                while not done:
+                    render = driver.render()
+                    out.write(render)
+                    if policy:
+                        action = policy(torch.as_tensor(obs, device=device).unsqueeze(0))[0].cpu().numpy()
+                    else: # random action
+                        action = driver.action_space.sample()
 
-env.close()
+                    obs, reward, terminated, truncated, info = env.step(action)
+                    done = terminated or truncated
+                    episodic_return += reward
+
+                    if terminated or truncated:
+                        f.write(f"info: {info}, total reward: {episodic_return}\n")
+                        episodic_returns.append(episodic_return)
+                        episodic_return = 0
+                        pbar.update(1)
+                        done = True
+                        env.reset()
+        returns = np.array(episodic_returns)
+        
+        f.write(f"Total reward: {np.sum(returns)}\n")
+        f.write(f"mean: {np.mean(returns)}\n")
+        f.write(f"std: {np.std(returns)}\n")
+        f.write(f"max: {np.max(returns)}\n")
+        f.write(f"min: {np.min(returns)}\n")
+        f.write("*" * 50 + "\n")
+        out.release()
+        cv2.destroyAllWindows()
+        env.close()
